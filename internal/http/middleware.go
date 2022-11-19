b43a292a214a9ab30ba139da6d27d3ccf028bfb9
melinaco4 <melinaco4@gmail.com>
2022-11-20 01:58:09 +0200

    comments added

--- internal/http/middleware.go
+++ internal/http/middleware.go
@@ -15,6 +15,7 @@ func JSONMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+//time our for requests that take a certain amount of time
 func TimeoutMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithTimeout(r.Context(), 15*time.Second)
@@ -23,7 +24,7 @@ func TimeoutMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-// LoggingMiddleware - a handy middleware function that logs out incoming requests
+// logging icnoming requests
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.WithFields(
