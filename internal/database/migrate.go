e268b0e769c19520db27e60bfc62f9da034e9c60
melinaco4 <melinaco4@gmail.com>
2022-11-19 19:56:03 +0200

    fixed migration

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -1,14 +1,48 @@
 package database
 
 import (
-	"github.com/jinzhu/gorm"
-	"github.com/melinaco4/companies-manager/internal/company"
+	"errors"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
-// MigrateDB - migrates our database and creates our comment table
+// MigrateDB - migrates our database and creates our companies table
+/*
 func MigrateDB(db *gorm.DB) error {
 	if result := db.AutoMigrate(&company.Company{}); result.Error != nil {
 		return result.Error
 	}
+	return nil
+}
+*/
+
+func (d *Database) MigrateDB() error {
+	fmt.Println("migrating our database")
+
+	driver, err := postgres.WithInstance(d.Client.DB, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("could not create the postgres driver: %w", err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file:///migrations",
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if err := m.Up(); err != nil {
+		if !errors.Is(err, migrate.ErrNoChange) {
+			return fmt.Errorf("could not run up migrations: %w", err)
+		}
+	}
+
+	fmt.Println("successfully migrated the database")
 	return nil
 }
---
+++ migrations/0001_create_companies_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS companies (
+    ID uuid,
+    Name text,
+    Description text,
+    AmountofEmployees int,
+    Registered boolean,
+    Type text
+);
